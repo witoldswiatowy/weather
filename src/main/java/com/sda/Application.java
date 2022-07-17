64511be344c2b751152e9aed632fdabf19394820
Michal Paukszto <michal.paukszto@gmail.com>
2022-07-17 12:51:16 +0200

    michal's commit

--- src/main/java/com/sda/Application.java
+++ src/main/java/com/sda/Application.java
@@ -3,6 +3,7 @@ package com.sda;
 public class Application {
     public static void main(String[] args) {
         System.out.println("Hello world");
+        System.out.println("Michał Paukszto");
     }
 
 }
